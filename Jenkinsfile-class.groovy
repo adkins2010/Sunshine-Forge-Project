daea8c80f8e8bf9f79722bf9bc990d61f4c4191b
localadmin <michael.michinobu.adkins@gmail.com>
2017-04-11 11:10:13 -0400

    Changed the Jenkinsfile-class.groovy to be configured to run on Dockers container

--- Jenkinsfile-class.groovy
+++ Jenkinsfile-class.groovy
@@ -1,9 +1,14 @@
 #!/usr/bin/env groovy
+def docker_registry
 
 node {
+    docker_registry = env.DOCKER_REGISTRY
     checkout scm
+}
+
+docker.image(docker_registry + "/compozed/ci-base:0.8").inside() {
+    env.GRADLE_USER_HOME="."
     stage("Build") {
-        env.GRADLE_USER_HOME="."
         sh '''
       ./gradlew clean build
       '''
