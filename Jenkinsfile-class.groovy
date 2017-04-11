1bd729af78e6077c81d9b42863796613446119d4
localadmin <michael.michinobu.adkins@gmail.com>
2017-04-11 15:04:51 -0400

    chang jenkinsfile with 2 credentials''

--- Jenkinsfile-class.groovy
+++ Jenkinsfile-class.groovy
@@ -10,7 +10,7 @@ docker.image(docker_registry + "/compozed/ci-base:0.8").inside() {
     env.GRADLE_USER_HOME = "."
     stage("Build") {
         sh '''
-      ./gradlew clean build -S
+      ./gradlew clean build
       '''
     }
 
@@ -28,8 +28,23 @@ docker.image(docker_registry + "/compozed/ci-base:0.8").inside() {
         set -e +x
         cf login -a api.cf.nonprod-mpn.ro11.allstate.com -u ${CF_USERNAME} -p ${CF_PASSWORD} --skip-ssl-validation; cf target -o IS-COMPOZED-ACCELERATOR -s DEV
         cf push -p build/libs/sunshine-forge-0.0.1-SNAPSHOT.jar
-        ./gradlew publish
         '''
+        }
+    }
+
+
+    withCredentials([
+            [
+                    $class          : 'UsernamePasswordMultiBinding',
+                    credentialsId   : 'c47a385f-6585-40c5-85ba-3cf2580e2776',
+                    passwordVariable: 'ARTIFACTORY_PASSWORD',
+                    usernameVariable: 'ARTIFACTORY_USERNAME'
+            ]]) {
+        stage("Publish to Artifactory") {
+            sh '''
+              set -e +x
+              ./gradlew publish
+            '''
         }
     }
 }
