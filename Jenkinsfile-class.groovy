be968dfe88051700a62534445d0e834e09f1a14d
localadmin <michael.michinobu.adkins@gmail.com>
2017-04-11 09:20:17 -0400

    Jenkins Buid File

---
+++ Jenkinsfile-class.groovy
@@ -0,0 +1,11 @@
+  #!/usr/bin/env groovy
+
+    node {
+        checkout scm
+            stage("Build") {
+                  env.GRADLE_USER_HOME="."
+                        sh '''
+                              ./gradlew clean build
+                                    '''
+                                        }
+                                          }
