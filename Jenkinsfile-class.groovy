f25c9724e8eebbef2eaff53302b5ca8382e16d71
localadmin <michael.michinobu.adkins@gmail.com>
2017-04-11 13:51:12 -0400

    Modified the manifest.yml to have a modified 'cf push' command

--- Jenkinsfile-class.groovy
+++ Jenkinsfile-class.groovy
@@ -7,27 +7,28 @@ node {
 }
 
 docker.image(docker_registry + "/compozed/ci-base:0.8").inside() {
-    env.GRADLE_USER_HOME="."
+    env.GRADLE_USER_HOME = "."
     stage("Build") {
         sh '''
       ./gradlew clean build
       '''
     }
-}
 
-withCredentials([
-        [
-                $class          : 'UsernamePasswordMultiBinding',
-                credentialsId   : 'c47a385f-6585-40c5-85ba-3cf2580e2776',
-                passwordVariable: 'CF_PASSWORD',
-                usernameVariable: 'CF_USERNAME'
-        ]]) {
 
-    stage("Deploy") {
-        sh '''
+    withCredentials([
+            [
+                    $class          : 'UsernamePasswordMultiBinding',
+                    credentialsId   : 'c47a385f-6585-40c5-85ba-3cf2580e2776',
+                    passwordVariable: 'CF_PASSWORD',
+                    usernameVariable: 'CF_USERNAME'
+            ]]) {
+
+        stage("Deploy") {
+            sh '''
         set -e +x
         cf login -a api.cf.nonprod-mpn.ro11.allstate.com -u ${CF_USERNAME} -p ${CF_PASSWORD} --skip-ssl-validation; cf target -o IS-COMPOZED-ACCELERATOR -s DEV
-        cf push
+        cf push -p build/libs/sunshine-forge-0.0.1-SNAPSHOT.jar
         '''
+        }
     }
 }
